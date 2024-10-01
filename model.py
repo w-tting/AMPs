532ea2095d6432857345fc03d21e2eb258b0a23d
w-tting <twting@mail.ustc.edu.cn>
2024-10-01 18:39:36 +0800

    file commit

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from keras.layers import Conv1D, ZeroPadding1D, MaxPooling1D, Dropout, Flatten, Dense, LSTM
 
 
-def cnn_regressor():
+def cnn_model():
     model = keras.models.Sequential()
     model.add(ZeroPadding1D(5, input_shape=(MAX_SEQUENCE_LENGTH, character_to_index)))
     model.add(Conv1D(64, kernel_size=5, strides=1, activation='relu'))
